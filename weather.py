abdf19cb3e8165461c78e233ba5df5ad737d09ed
snidarian <cephalopod31956@gmail.com>
2021-06-18 17:14:58 -0400

    forming up

--- weather.py
+++ weather.py
@@ -2,8 +2,24 @@
 
 
 
+import requests as rq
+import argparse
 
 
 
 
+parser = argparse.ArgumentParser(description="Outputs 3 day forcast for given location argument")
+
+args = parser.add_argument('location', help="city or zipcode string", type=str)
+
+args = parser.parse_args()
+
+
+result = rq.get(f'https://goweather.herokuapp.com/weather/{args.location}')
+
+
+data_object = result.json()
+
+
+print(data_object)
 
