bbf572bfc67d6159c80ce422f0703c1777e827b8
snidarian <cephalopod31956@gmail.com>
2021-06-18 17:49:05 -0400

    forming up

--- weather.py
+++ weather.py
@@ -11,6 +11,7 @@ import argparse
 parser = argparse.ArgumentParser(description="Outputs 3 day forcast for given location argument")
 
 args = parser.add_argument('location', help="city or zipcode string", type=str)
+args = parser.add_argument('--c', '--days-ahead', help='Specify how many days ahead the forcast should be (max 3)', type=str)
 
 args = parser.parse_args()
 
